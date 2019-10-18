8ec68cfc380bc2fa8ff1dd337be45c21be92ab60
surajjadli <surajjadli3@gmail.com>
2019-10-18 21:41:39 +0530

    Updates

---
+++ precache-manifest.0e44789f13f522e5f99ce3a4816bae46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329583b3f24f909f750843ec304a46ec",
+    "url": "/friendsapp/index.html"
+  },
+  {
+    "revision": "9b6725fbf130a4a5b48b",
+    "url": "/friendsapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "26bde170d1ec53b8a7e5",
+    "url": "/friendsapp/static/css/main.4113b39d.chunk.css"
+  },
+  {
+    "revision": "9b6725fbf130a4a5b48b",
+    "url": "/friendsapp/static/js/2.2d2f3f6c.chunk.js"
+  },
+  {
+    "revision": "26bde170d1ec53b8a7e5",
+    "url": "/friendsapp/static/js/main.96bd1a90.chunk.js"
+  },
+  {
+    "revision": "69c0df56010dd77b4bbe",
+    "url": "/friendsapp/static/js/runtime-main.857242d4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendsapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
